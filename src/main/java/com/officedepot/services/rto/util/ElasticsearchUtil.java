8925dcac3272059898caf58ece8903c4d61e7180
Renae Frearson <Renae-Frearson@na.odcorp.net>
2022-06-22 15:39:26 -0400

    ODNA-205862 changed logging level

--- src/main/java/com/officedepot/services/rto/util/ElasticsearchUtil.java
+++ src/main/java/com/officedepot/services/rto/util/ElasticsearchUtil.java
@@ -49,7 +49,7 @@ public class ElasticsearchUtil {
 		
 	public void writeDeadLetterToElasticsearchIndex(String msg, String json){
 		
-		logger.error(CLASS_NAME + "writeDeadLetterToElasticsearchIndex" + METHOD_MESSAGE + ES_DEADLETTER_INDEX);
+		logger.debug(CLASS_NAME + "writeDeadLetterToElasticsearchIndex" + METHOD_MESSAGE + ES_DEADLETTER_INDEX);
 		PayloadUtil payloadUtil = new PayloadUtil();
 		
 		if ((ES_DEADLETTER_INDEX_ENABLED) 
